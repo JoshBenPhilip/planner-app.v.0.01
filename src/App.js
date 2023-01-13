b00a1df318b715a27ea7f691de30d0bbf8171364
JoshBenPhilip <joshben4c@gmail.com>
2023-01-12 23:06:00 -0500

    Added Title (h1), Date (h1), and content (p) elements to PlannerPageThurJan_12 component

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
     <>
       <div className="App">
         <header className="App-header">
+          <PlannerPageThurJan_12></PlannerPageThurJan_12>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <PlannerPageThurJan_12></PlannerPageThurJan_12>
           <a
             className="App-link"
             href="https://reactjs.org"
