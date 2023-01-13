9f15580cbf848e7966421125cae2fd34c546d1e2
JoshBenPhilip <joshben4c@gmail.com>
2023-01-12 23:35:11 -0500

    Added Planner Page Template

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import PlannerPageThurJan_12 from "./PlannerPageThurJan_12";
+import PlannerPageFriJan_13 from "./PlannerPageFriJan_13";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <PlannerPageThurJan_12></PlannerPageThurJan_12>
+          <PlannerPageFriJan_13></PlannerPageFriJan_13>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
---
+++ src/PlannerPageFriJan_13.jsx
@@ -0,0 +1,10 @@
+export default function PlannerPageFriJan_13() {
+  return (
+    <>
+      <h1>Planner</h1>
+
+      <h2>Date: Friday, January 13, 2023</h2>
+      <p>11:05 - sleep</p>
+    </>
+  );
+}
